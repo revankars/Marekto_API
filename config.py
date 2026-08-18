65b42d73cf1e18c9eb82aaa35734c168789fc6b4
Sainath Revankar <sainathrevankar1401@gmail.com>
2026-08-18 11:48:24 -0600

    Replace Marketo API credentials with placeholders
    Updated Marketo API credentials and base URL placeholders.

--- config.py
+++ config.py
@@ -3,18 +3,18 @@
 # Marketo REST API endpoint base URL.
 # Example:
 # https://123-ABC-456.mktorest.com
-MARKETO_BASE_URL = "https://235-vbq-065.mktorest.com"
+MARKETO_BASE_URL = "https://<marketoapi>.mktorest.com"
 
 # OAuth credentials from Marketo LaunchPoint.
-CLIENT_ID = "81f0bbaa-dd24-4a4e-8014-c414d580956f"
-CLIENT_SECRET = "82EACMz6PS60LlATzLJ8ytsZtUBjEfqf"
+CLIENT_ID = "client-id"
+CLIENT_SECRET = "client-secret"
 
 # If an access token has already been provided to you,
 # put it here.
 #
 # If this is None/empty, the script will obtain a new token
 # using CLIENT_ID and CLIENT_SECRET.
-ACCESS_TOKEN = "f9692f20-02c1-409b-a9e7-ea5a2de8cd3f:or2"
+ACCESS_TOKEN = "access token"
 
 # CSV file to import
 CSV_FILE = "lead_data.csv"
